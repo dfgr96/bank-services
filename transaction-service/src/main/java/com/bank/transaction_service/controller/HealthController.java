c11218322fee74d074c9e08d8c78b2e86ca30fbc
David Felipe Gomez Rodriguez <davidfelgomezr@hotmail.com>
2025-03-28 06:29:06 -0500

    Feat: add transaction-service in proyect

--- transaction-service/src/main/java/com/bank/transaction_service/controller/HealthController.java
+++ transaction-service/src/main/java/com/bank/transaction_service/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.bank.account_service.controller;
+package com.bank.transaction_service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +12,7 @@ public class HealthController {
 
     @GetMapping
     public Map<String, String> checkHealth() {
-        return Map.of("status account-service", "UP");
+        return Map.of("status transaction-service", "UP");
     }
 
 }
