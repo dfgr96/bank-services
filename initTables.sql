2d43d51c7a9b80aba09e17fc76355c6fc748dc2a
David Felipe Gomez Rodriguez <davidfelgomezr@hotmail.com>
2025-03-28 20:12:44 -0500

    Feature/proyect structure (#1)
    * feat: first commit, proyect structuring
    
    * Feat: add git ignore
    
    * Feat: add transaction-service in proyect
    
    * Feat: add file to create tables

---
+++ initTables.sql
@@ -0,0 +1,17 @@
+-- Crear la tabla de cuentas
+CREATE TABLE accounts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    saldo DECIMAL(10,2) NOT NULL DEFAULT 0.00
+);
+
+-- Crear la tabla de transacciones
+CREATE TABLE transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    from_account INT NOT NULL,
+    to_account INT NOT NULL,
+    monto DECIMAL(10,2) NOT NULL,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (from_account) REFERENCES accounts(id) ON DELETE CASCADE,
+    FOREIGN KEY (to_account) REFERENCES accounts(id) ON DELETE CASCADE
+);
---
+++ transaction-service/src/main/java/com/bank/transaction_service/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.bank.transaction_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public Map<String, String> checkHealth() {
+        return Map.of("status transaction-service", "UP");
+    }
+
+}
