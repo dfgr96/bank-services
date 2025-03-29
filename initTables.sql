07ba9d9cf80fb75a69c25b6664d342156ea6b49f
David Felipe Gomez Rodriguez <davidfelgomezr@hotmail.com>
2025-03-29 16:49:17 -0500

    feat: add sh file to start project

--- initTables.sql
+++ initTables.sql
@@ -1,12 +1,14 @@
--- Crear la tabla de cuentas
-CREATE TABLE bankdb.accounts (
+USE bankdb;
+
+-- Crear la tabla de cuentas si no existe
+CREATE TABLE IF NOT EXISTS accounts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     saldo DECIMAL(10,2) NOT NULL DEFAULT 0.00
 );
 
--- Crear la tabla de transacciones
-CREATE TABLE bankdb.transactions (
+-- Crear la tabla de transacciones si no existe
+CREATE TABLE IF NOT EXISTS transactions (
     id INT AUTO_INCREMENT PRIMARY KEY,
     from_account INT NOT NULL,
     to_account INT NOT NULL,
